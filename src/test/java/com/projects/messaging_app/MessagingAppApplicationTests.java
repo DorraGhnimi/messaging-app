206fadad114ce2132e8d68efb6691c77a86416cf
DorraGhnimi <21196213+DorraGhnimi@users.noreply.github.com>
2024-09-03 16:33:24 +0100

    initial commit + github Oauth

---
+++ src/test/java/com/projects/messaging_app/MessagingAppApplicationTests.java
@@ -0,0 +1,12 @@
+package com.projects.messaging_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MessagingAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+}
