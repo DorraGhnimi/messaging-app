748fd1ef7a36fe43aec464accfa06efe51c46abf
DorraGhnimi <21196213+DorraGhnimi@users.noreply.github.com>
2024-09-03 16:49:45 +0100

    authorization

--- src/main/java/com/projects/messaging_app/messaging/MessagingAppApplication.java
+++ src/main/java/com/projects/messaging_app/messaging/MessagingAppApplication.java
@@ -1,11 +1,14 @@
 package com.projects.messaging_app;
 
+import com.projects.messaging_app.messaging.DataStaxAstraProperties;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.security.oauth2.core.user.OAuth2User;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.boot.autoconfigure.cassandra.CqlSessionBuilderCustomizer;
+import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.nio.file.Path;
 
 @SpringBootApplication
 @RestController
@@ -15,10 +18,18 @@ public class MessagingAppApplication {
 		SpringApplication.run(MessagingAppApplication.class, args);
 	}
 
+	/*
 	@RequestMapping("/user")
 	public String user(@AuthenticationPrincipal OAuth2User principal) {
 		System.out.println(principal);
 		return principal.getAttribute("name");
+	}*/
+
+	@Bean
+	public CqlSessionBuilderCustomizer sessionBuilderCustomizer(DataStaxAstraProperties astraProperties) {
+		Path bundle = astraProperties.getSecureConnectBundle().toPath();
+		return builder -> builder.withCloudSecureConnectBundle(bundle);
 	}
+
 
 }
